9d962e01848faa03af802a9b94384fb56cb5948e
Christian <christian.lei1@gmail.com>
2015-11-09 23:55:53 -0800

    adds styling to multiple images

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,6 @@
 class Photo < ActiveRecord::Base
-  has_attached_file :image, styles: { listing: "500x500>", medium: "300x300>", main: "200x200>", thumb: "100x100>" }, url: "/images/:id/:filename", path: ":rails_root/public/images/:id/:filename", default_url: "/images/:style/missing.png"
-
-  do_not_validate_attachment_file_type :image
+  has_attached_file :image, styles: { listing: "500x500>", medium: "300x300>", main: "200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   belongs_to :post
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,7 @@ class Post < ActiveRecord::Base
   has_and_belongs_to_many :tags
   belongs_to :user
   belongs_to :cart
-  has_many :comments
+  has_many :comments, :dependent => :destroy
   has_many :photos, :dependent => :destroy
-
 
 end
