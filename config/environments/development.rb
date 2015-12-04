cdc8b157c1312bac190e119ae9dff7d2e3418442
Christian <christian.lei1@gmail.com>
2015-12-04 11:13:53 -0800

    s3 works in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,6 +48,8 @@ config.paperclip_defaults = {
   :bucket => ENV['S3_BUCKET_NAME'],
   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-  }
+},
+url: ':s3_domain_url',
+path: '/:class/:attachment/:id_partition/:style/:filename'
 }
 end
