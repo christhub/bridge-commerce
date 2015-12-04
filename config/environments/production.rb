1915a6d82b00bdc18f3791a3cddf097060b1867a
Christian <christian.lei1@gmail.com>
2015-12-03 22:50:53 -0800

    environment variables DON'T work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,11 +80,11 @@ Rails.application.configure do
   #config paperclip defaults
   config.paperclip_defaults = {
   :storage => :s3,
+  :s3_region => ENV['AWS_REGION'],
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region => ENV['AWS_REGION'],
     }
   }
 end
